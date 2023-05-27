f7a1f7de28a03c34483afd3cd4f7efcf09fe2faa
robiokidenis <robiokidenis@gmail.com>
2023-05-27 13:47:19 +0700

    update seeder add another user

--- Backend/database/seeders/UserSeeder.php
+++ Backend/database/seeders/UserSeeder.php
@@ -12,7 +12,7 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $user = User::create([
+        $user1 = User::create([
             'first_name' => 'Robioki',
             'last_name' => 'Denis',
             'country' => 'United States',
@@ -22,12 +22,28 @@ class UserSeeder extends Seeder
             'phone_number' => '+(12)3456 789',
             'birthday' => '1990-08-15',
             'organization' => 'Company Name',
-            'role' => 'Software Engineeer',
+            'role' => 'Software Engineer',
             'department' => 'Development',
             'zip_code' => '12345',
             'password' => bcrypt('password'),
         ]);
-
+        
+        $user2 = User::create([
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'country' => 'Canada',
+            'city' => 'Toronto',
+            'address' => 'Ontario',
+            'email' => 'johndoe@example.com',
+            'phone_number' => '+(34)5678 901',
+            'birthday' => '1995-05-20',
+            'organization' => 'Company XYZ',
+            'role' => 'Project Manager',
+            'department' => 'Management',
+            'zip_code' => '54321',
+            'password' => bcrypt('password'),
+        ]);
+        
         // Generate additional random users
         User::factory()->count(10)->create();
         // BankAccount::create([
